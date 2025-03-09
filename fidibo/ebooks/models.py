7ff5d7a8ced03318093282bc721f6eca04390935
taherihamed <taheri.hamed70@gmail.com>
2025-03-09 18:07:36 +0330

    add book models

--- fidibo/ebooks/models.py
+++ fidibo/ebooks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_id = models.CharField(max_length=12, unique=True)
+    book_name = models.CharField(max_length=100)
+    book_author = models.CharField(max_length=100)
+    book_rate = models.FloatField(max_length=3)
+    book_price = models.CharField(max_length=15)
+    book_translator = models.CharField(max_length=100)
+    book_publisher = models.CharField(max_length=100)
+
+
